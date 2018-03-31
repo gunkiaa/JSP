6b4d38a1ec722d56b2cda0f3fc848b9322adf64c
kimgun <rlarjs4456@sungil-i.kr>
2018-03-31 20:30:42 +0900

    JSP MVC 패턴 연습
    SQL파일 교체

--- BoardMVC/WebContent/MVC.sql
+++ BoardMVC/WebContent/MVC.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: 127.0.0.1    Database: board
 -- ------------------------------------------------------
--- Server version	5.7.21-log
+-- Server version	5.7.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -39,7 +39,34 @@ CREATE TABLE `mvc_board` (
 
 LOCK TABLES `mvc_board` WRITE;
 /*!40000 ALTER TABLE `mvc_board` DISABLE KEYS */;
+INSERT INTO `mvc_board` VALUES (2,'김건','안녕','김건','2018-03-31 04:43:40',0),(5,'안녕','김건','김건','2018-03-31 04:50:04',2),(6,'김건입니다','내일 모레 학교에 갑니다.','김건','2018-03-31 04:50:36',0);
 /*!40000 ALTER TABLE `mvc_board` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mvc_user`
+--
+
+DROP TABLE IF EXISTS `mvc_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mvc_user` (
+  `uID` varchar(20) NOT NULL,
+  `uPW` varchar(20) NOT NULL,
+  `uName` varchar(45) NOT NULL,
+  `uGender` varchar(45) NOT NULL,
+  `uEmail` varchar(45) NOT NULL,
+  PRIMARY KEY (`uID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mvc_user`
+--
+
+LOCK TABLES `mvc_user` WRITE;
+/*!40000 ALTER TABLE `mvc_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mvc_user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +78,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-31 13:33:04
+-- Dump completed on 2018-03-31 20:29:50
